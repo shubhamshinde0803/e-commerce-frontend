e12b527287669ac6ff55cea3c5eb364a85e7837b
Shubham Shinde <shubhamshinde0803@gmail.com>
2023-11-23 15:26:51 +0530

    product page implementation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Navigation from "./customer/components/Navigation/Navigation";
 import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
+import Product from "./customer/components/Product/Product";
 
 function App() {
   return (
     <div className="">
       <Navigation />
+      <Product/>
       <HomePage />
       <Footer />
     </div>
