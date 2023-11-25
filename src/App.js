0022dfcff85861c5e9add955ba685ee9e012ba53
Shubham Shinde <shubhamshinde0803@gmail.com>
2023-11-25 18:13:37 +0530

    implemented checkout and cart feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
 import Product from "./customer/components/Product/Product";
 import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
+import Cart from "./customer/components/Cart/Cart";
+import Checkout from "./customer/components/Checkout/Checkout";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       <Navigation />
       {/* <Product/> */}
       {/* <HomePage /> */}
-      <ProductDetails />
+      {/* <ProductDetails /> */}
+      {/* <Cart /> */}
+      <Checkout/>
       <Footer />
     </div>
   );
---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+        <div className='space-y-3'>
+            <p className='font-semibold'>Ram Kapoor</p>
+            <p>Mumbai, gokul dham market, 400001</p>
+            <div>
+                <p className='font-semibold'>Phone Number</p>
+                <p>3224144522</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddressCard
