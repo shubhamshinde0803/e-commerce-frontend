47282ca5e67d9a71ffb4e4010ee5fc84463d6377
Shubham Shinde <shubhamshinde0803@gmail.com>
2023-11-25 14:56:43 +0530

    implementation of product details page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navigation from "./customer/components/Navigation/Navigation";
 import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
 import Product from "./customer/components/Product/Product";
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
 
 function App() {
   return (
     <div className="">
       <Navigation />
-      <Product/>
-      <HomePage />
+      {/* <Product/> */}
+      {/* <HomePage /> */}
+      <ProductDetails />
       <Footer />
     </div>
   );
