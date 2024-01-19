bf7d2cd53e1477fa76edee7f21276f6272279286
Shubham Shinde <shubhamshinde0803@gmail.com>
2024-01-19 17:57:36 +0530

    error resolved

--- src/State/Auth/Action.js
+++ src/State/Auth/Action.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { API_BASE_URL } from "../../config/apiConfig";
 import {
-    GET_USER_FAILURE,
-    GET_USER_REQUEST,
-    GET_USER_SUCCESS,
-    LOGIN_FAILURE,
-    LOGIN_REQUEST,
+  GET_USER_FAILURE,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
   LOGIN_SUCCESS,
+  LOGOUT,
   REGISTER_FAILURE,
   REGISTER_REQUEST,
   REGISTER_SUCCESS,
@@ -37,40 +38,39 @@ const loginSuccess = (user) => ({ type: LOGIN_SUCCESS, payload: user });
 const loginFailure = (error) => ({ type: LOGIN_FAILURE, payload: error });
 
 export const login = (userData) => async (dispatch) => {
-    dispatch(loginRequest());
-    try {
-      const response = await axios.post(`${API_BASE_URL}/auth/signin`, userData);
-      const user = response.data;
-      if (user.jwt) {
-        localStorage.setItem("jwt", user.jwt);
-      }
-      dispatch(loginSuccess);
-    } catch (error) {
-      dispatch(loginFailure(error.message));
+  dispatch(loginRequest());
+  try {
+    const response = await axios.post(`${API_BASE_URL}/auth/signin`, userData);
+    const user = response.data;
+    if (user.jwt) {
+      localStorage.setItem("jwt", user.jwt);
     }
+    dispatch(loginSuccess);
+  } catch (error) {
+    dispatch(loginFailure(error.message));
+  }
 };
 
-  const getUserRequest = () => ({ type: GET_USER_REQUEST });
+const getUserRequest = () => ({ type: GET_USER_REQUEST });
 const getUserSuccess = (user) => ({ type: GET_USER_SUCCESS, payload: user });
 const getUserFailure = (error) => ({ type: GET_USER_FAILURE, payload: error });
 
 export const getUser = () => async (dispatch) => {
-    dispatch(getUserRequest());
-    try {
-      const response = await axios.get(`${API_BASE_URL}/api/users/profile`, headers:{
-        headers:{
-            "Authorization":`Bearer ${token}`
-        }
-      });
-      const user = response.data;
-      
-      
-      dispatch(getUserSuccess(user));
-    } catch (error) {
-      dispatch(getUserFailure(error.message));
-    }
-  };
+  dispatch(getUserRequest());
+  try {
+    const response = await axios.get(`${API_BASE_URL}/api/users/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const user = response.data;
 
-  export const logout = () => (dispatch)=> {
-    dispatch({type:LOGOUT, payload:null})
+    dispatch(getUserSuccess(user));
+  } catch (error) {
+    dispatch(getUserFailure(error.message));
   }
+};
+
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT, payload: null });
+};
